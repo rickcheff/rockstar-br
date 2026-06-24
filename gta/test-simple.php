10b8fe03b42d4a3d36b93b5fa7600d85ea89db5e
rick <rickdamirars@gmail.com>
2026-06-24 15:19:20 -0300

    chore: remove deprecated PIX payment integration files

--- gta/test-simple.php
+++
@@ -1,4 +0,0 @@
-﻿<?php
-file_put_contents(__DIR__ . '/test-output.txt', "PHP funcionando - " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
-echo "OK";
-?>
--- gta/test.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-echo json_encode([
-    'status' => 'ok',
-    'php_version' => phpversion(),
-    'timestamp' => date('Y-m-d H:i:s')
-]);
-?>
